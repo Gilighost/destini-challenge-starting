7460522c6492559618177c6445172dbff3f124b8
Cameron LaFerney <chlaferney@gmail.com>
2020-05-02 15:22:00 -0500

    Add StoryMap class and update StoryBrain.

---
+++ lib/storyMap.dart
@@ -0,0 +1,9 @@
+import 'story.dart';
+
+class StoryMap {
+  Story story;
+  int choice1Destination;
+  int choice2Destination;
+
+  StoryMap({this.story, this.choice1Destination, this.choice2Destination});
+}
